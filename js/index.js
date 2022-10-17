c8855fe90110f84f5323442ab371e75e6e65e8a0
Lorelei Leon <pamelaorte05@gmail.com>
2022-10-17 10:47:51 -0600

    final

--- js/index.js
+++ js/index.js
@@ -1,22 +1,37 @@
 //declarar variables para vincular los objetos de formulario
-const usuario =  document.getElementById('user');
+const usuario = document.getElementById('user');
 const password = document.getElementById('pass');
-const formulario = document.getElementById('formLogin');
+const formulario= document.getElementById('formLogin');
 
-//Genereando eventos
+//Generando eventos
 formulario.addEventListener('submit', login);
 
 //funciones a ejecutar
 function login(e){
     e.preventDefault();
-
-    let usuarioVal = usuario.value;
-    let passwordVal = password.value;
+    
+    let usuarioVal= usuario.value;
+    let passwordVal= password.value;
 
     if(usuarioVal == '' || passwordVal == ''){
+        creaMensaje('Verifica tus campos', 'danger')
         return;
-    } 
+    }
 
-    console.log('El valor para user es:' + usuarioVal);
-    console.log('El valor para pass es:' + passwordVal);
+    if(localStorage.getItem('usuario')){
+        let objeto = JSON.parse(localStorage.getItem('usuario'));
+
+        if(usuarioVal == objeto.user && passwordVal == objeto.pass ){
+            creaMensaje('Usuario correcto', 'success')
+            setTimeout(function(){
+                window.open('./inicio.html', '_self');
+            },2000);
+
+        }else{
+            creaMensaje('Usuario Incorrecto' , 'danger')
+        }
+    }else{
+        creaMensaje('No har registros' , 'danger')
+    }
+
 }
--- js/registro.js
+++ js/registro.js
@@ -1,25 +1,43 @@
-//declarar variables para vincular los objetos de formulario
-const nombre =  document.getElementById('name');
-const email =  document.getElementById('email');
-const user =  document.getElementById('user');
+//Declaracion de variables de referencia a objetos
+const nombre = document.getElementById('name');
+const email = document.getElementById('email');
+const user = document.getElementById('user');
 const pass = document.getElementById('pass');
 const formulario = document.getElementById('formRegistro');
 
-//Genereando eventos
 formulario.addEventListener('submit', registro);
 
-//funciones a ejecutar
-function login(e){
+function registro(e){
     e.preventDefault();
+    
 
     let nombreVal = nombre.value;
     let emailVal = email.value;
     let userVal = user.value;
-    let passwordVal = password.value;
+    let passValue = pass.value;
 
-    if(nombreVal == '' || emailVal == '' || userVal == '' || passwordVal == ''){
+    if(nombreVal == '' || emailVal =='' || userVal == '' || passValue == ''){
+       
+        creaMensaje('Verifica tus campos!', 'danger');
         return;
-    } 
+    }
 
-    console.log('valindando datos!');
+    const usuario = {
+        nombre: nombreVal,
+        email: emailVal,
+        user: userVal, 
+        pass: passValue
+    }
+
+    localStorage.setItem('usuario', JSON.stringify(usuario));
+
+    
+    nombre.value = ''
+    email.value = ''
+    user.value = ''
+    pass.value = ''
+
+
+    creaMensaje('Usuario Registrado', 'success')
 }
+
